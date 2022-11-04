f5d8ab139819090a0318d896fc8700d7d843f028
Yasin <yasin.avci@outlook.com>
2022-11-04 17:12:36 +0100

    fix conflicts

--- src/lib/components/molecules/index.tsx
+++ src/lib/components/molecules/index.tsx
@@ -1,7 +1,4 @@
+export * from './ListGroup';
 export * from './Modal';
 export * from './Tab';
-<<<<<<< HEAD
-export * from './ListGroup';
-=======
 export * from './Timeline';
->>>>>>> eb51a1a (Move Timeline component (without .theme))
