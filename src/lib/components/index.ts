5bcb4a331476ca49e4fb5242c1037aeadf50728d
Murat Avcı <murat.avci95@gmail.com>
2022-11-05 17:30:00 +0100

    Export modals

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -6,13 +6,13 @@ export * from './Card';
 export * from './Carousel';
 export * from './Dropdown';
 export * from './Footer';
+export * from './molecules';
 export * from './Navbar';
 export * from './Pagination';
 export * from './Rating';
 export * from './Sidebar';
 export * from './Spinner';
 export * from './Table';
-
 
 export type DeepPartial<T> = T extends object
   ? {
