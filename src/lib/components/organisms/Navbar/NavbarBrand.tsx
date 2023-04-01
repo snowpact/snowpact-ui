12b9939443b7df3e64af3e5e658b97d1e283cea2
Murat Avcı <murat.avci95@gmail.com>
2023-04-01 18:33:16 +0200

    fix(navbar): close navbar on mobile for brand

--- src/lib/components/organisms/Navbar/NavbarBrand.tsx
+++ src/lib/components/organisms/Navbar/NavbarBrand.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps, FC, PropsWithChildren } from 'react';
 import { excludeClassName } from '../../../helpers/exclude';
 import { useTheme } from '../../bosons/HelloInternet/ThemeContext';
+import { useNavbarContext } from './NavbarContext';
 
 export type NavbarBrandProps = Omit<PropsWithChildren<ComponentProps<'a'>>, 'className'> & {
   as?: React.ElementType;
@@ -11,8 +12,17 @@ export const NavbarBrand: FC<NavbarBrandProps> = ({ children, href, as, ...props
   const theirProps = excludeClassName(props);
   const LinkComponent = as || 'a';
 
+  const { setIsOpen } = useNavbarContext();
+
+  const onClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    if (props.onClick) {
+      props.onClick(e);
+    }
+    setIsOpen(false);
+  };
+
   return (
-    <LinkComponent href={href} className={theme.brand} {...theirProps}>
+    <LinkComponent href={href} className={theme.brand} {...theirProps} onClick={onClick}>
       {children}
     </LinkComponent>
   );
