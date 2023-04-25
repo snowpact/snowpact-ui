5f7e9f98fcfd962d61df9440e4ba5c46fea9654e
Anthony HOANG <anthonyhoang@MBP-de-Anthony.lan>
2023-04-25 17:55:01 +0200

    feat(breadcrumb): component migration done

--- src/lib/components/atoms/Breadcrumb/Breadcrumb.theme.tsx
+++ src/lib/components/atoms/Breadcrumb/Breadcrumb.theme.tsx
@@ -1,25 +1,13 @@
 export type BreadcrumbTheme = {
-  item: {
-    base: string;
-    chevron: string;
-    href: {
-      off: string;
-      on: string;
-    };
-    icon: string;
-  };
-  list: string;
+  base: string;
+  text: string;
+  chevron: string;
+  href: string;
 };
 
 export const BREADCRUMB_THEME: BreadcrumbTheme = {
-  item: {
-    base: 'group flex items-center',
-    chevron: 'mx-1 h-6 w-6 text-gray-400 group-first:hidden md:mx-2',
-    href: {
-      off: 'flex items-center text-sm font-medium text-gray-500',
-      on: 'flex items-center text-sm font-medium text-gray-700 hover:text-gray-900:text-white'
-    },
-    icon: 'mr-2 h-4 w-4'
-  },
-  list: 'flex items-center'
+  base: 'bg-gray-50 px-5 py-3',
+  text: 'text-sm text-primary-500',
+  chevron: 'text-secondary-700',
+  href: 'text-tertiary-500'
 };
--- src/lib/components/atoms/Breadcrumb/BreadcrumbItem.tsx
+++ src/lib/components/atoms/Breadcrumb/BreadcrumbItem.tsx
@@ -1,27 +1,35 @@
-import type { ComponentProps, FC, PropsWithChildren } from 'react';
+import classNames from 'classnames';
+import type { FC } from 'react';
 import { HiOutlineChevronRight } from 'react-icons/hi';
-import { excludeClassName } from '../../../helpers/exclude';
+import { twMerge } from 'tailwind-merge';
+import { LinkOrDiv } from '../../bosons';
 import { useTheme } from '../../bosons/HelloInternet/ThemeContext';
 
-export interface BreadcrumbItemProps extends PropsWithChildren<Omit<ComponentProps<'li'>, 'className'>> {
+export interface BreadcrumbItemProps {
+  children: string;
   href?: string;
-  icon?: FC<ComponentProps<'svg'>>;
+  icon?: React.ReactNode;
+  as?: React.ElementType;
 }
 
-export const BreadcrumbItem: FC<BreadcrumbItemProps> = ({ children, href, icon: Icon, ...props }): JSX.Element => {
-  const isLink = typeof href !== 'undefined';
-  const theirProps = excludeClassName(props);
-  const theme = useTheme().theme.breadcrumb.item;
-
-  const Component = isLink ? 'a' : 'span';
+export const BreadcrumbItem: FC<BreadcrumbItemProps> = ({ children, href, icon, as }): JSX.Element => {
+  const theme = useTheme().theme.breadcrumb;
 
   return (
-    <li className={theme.base} {...theirProps}>
-      <HiOutlineChevronRight aria-hidden className={theme.chevron} data-testid="flowbite-breadcrumb-separator" />
-      <Component className={theme.href[isLink ? 'on' : 'off']} data-testid="flowbite-breadcrumb-item" href={href}>
-        {Icon && <Icon aria-hidden className={theme.icon} />}
+    <li className={twMerge(classNames('group flex items-center', theme.text))}>
+      <HiOutlineChevronRight
+        aria-hidden
+        className={twMerge(classNames('h-full group-first:hidden md:mx-2', theme.chevron))}
+        data-testid="sui-breadcrumb-separator"
+      />
+      <LinkOrDiv
+        linkComponent={as}
+        className={twMerge(classNames('flex items-center font-medium', href && theme.href))}
+        href={href}
+      >
+        {icon && <span className="mr-2">{icon}</span>}
         {children}
-      </Component>
+      </LinkOrDiv>
     </li>
   );
 };
