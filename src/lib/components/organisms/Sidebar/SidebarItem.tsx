f5eb50a6c5ecd71ccf8441c820160ce8b3c126af
Anthony HOANG <anthonyhoang@MBP-de-Anthony.lan>
2023-04-26 10:52:02 +0200

    feat(badge): ci error fix

--- src/lib/components/organisms/Sidebar/SidebarItem.tsx
+++ src/lib/components/organisms/Sidebar/SidebarItem.tsx
@@ -16,7 +16,7 @@ export interface SidebarItemProps
   href?: string;
   icon?: FC<ComponentProps<'svg'>>;
   label?: string;
-  labelColor?: keyof SidebarItemLabelColors;
+  //labelColor?: keyof SidebarItemLabelColors; //TODO:Use props or remove it
 }
 
 export interface SidebarItemLabelColors extends Pick<HIColors, 'gray'> {
@@ -29,7 +29,7 @@ export const SidebarItem: FC<SidebarItemProps> = ({
   icon: Icon,
   active: isActive,
   label,
-  labelColor = 'info',
+  //labelColor = 'info', //TODO:Use props or remove it
   ...props
 }) => {
   const theirProps = excludeClassName(props);
