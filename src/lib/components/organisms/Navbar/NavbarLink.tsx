5283903e85450d625ab158284a952cf5bdb049bc
Anthony HOANG <anthony.hoang@snowpact.com>
2023-09-27 11:01:09 +0200

    feat(navbar): conditionnal link made agnostic

--- src/lib/components/organisms/Navbar/NavbarLink.tsx
+++ src/lib/components/organisms/Navbar/NavbarLink.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import type { FC } from 'react';
-import { Link } from 'react-router-dom';
 import { useTheme } from '../../bosons/HelloInternet/ThemeContext';
 import { useNavbarContext } from './NavbarContext';
 
@@ -38,34 +37,22 @@ export const NavbarLink: FC<NavbarLinkProps> = ({
     setIsOpen(false);
   };
 
-  const ContainerComponent = as || 'div';
-  if (href) {
-    return (
-      <li className="group relative w-full whitespace-nowrap text-center md:w-fit md:text-left">
-        <Link
-          to={href}
-          className={classNames(theme.base, active && theme.active, disabled && theme.disabled)}
-          {...props}
-          onClick={handleOnClick}
-        >
-          <span className="w-full">
-            {children}
-            {withUnderlineEffect && (
-              <span className={classNames('absolute bottom-0 left-0 w-0 group-hover:w-full', theme.underline)}></span>
-            )}
-          </span>
-        </Link>
-      </li>
-    );
-  }
+  const ContainerComponent = href ? as ?? 'a' : 'div';
   return (
-    <ContainerComponent className={classNames(theme.base, active && theme.active, disabled && theme.disabled)}>
-      <span className="w-full">
-        {children}
-        {withUnderlineEffect && (
-          <span className={classNames('absolute bottom-0 left-0 w-0 group-hover:w-full', theme.underline)}></span>
-        )}
-      </span>
-    </ContainerComponent>
+    <li className="group relative w-full whitespace-nowrap text-center md:w-fit md:text-left">
+      <ContainerComponent
+        href={href}
+        className={classNames(theme.base, active && theme.active, disabled && theme.disabled)}
+        {...props}
+        onClick={handleOnClick}
+      >
+        <span className="w-full">
+          {children}
+          {withUnderlineEffect && (
+            <span className={classNames('absolute bottom-0 left-0 w-0 group-hover:w-full', theme.underline)}></span>
+          )}
+        </span>
+      </ContainerComponent>
+    </li>
   );
 };
